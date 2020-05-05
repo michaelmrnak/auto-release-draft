c2532d7d9e3d085a84400c331dd3c56d471962c7
Michael Mrnak <michael.mrnak@onepeloton.com>
2020-05-05 02:34:43 -0400

    Update function syntax

--- src/event.ts
+++ src/event.ts
@@ -1,15 +1,14 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 
-export const getCreatedTag = (): string | null => {
-  if (github.context.eventName !== 'create') {
-    core.info(`The event name was ${github.context.eventName}.`)
-
+export function getCreatedTag(): string | null {
+  if (github.context.eventName != 'create') {
+    core.info(`The event name was ${github.context.eventName}`)
     return null
   }
 
-  if (github.context.payload.ref_type !== 'tag') {
-    core.info(`The created reference was a branch, not a tag`)
+  if (github.context.payload.ref_type != 'tag') {
+    core.info('The created reference was a branch, not a tag')
     return null
   }
 
